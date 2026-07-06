f5e34300257094dbfc9899e964b4913eeaa0427f
Hiago 7k <hiago7k@gmail.com>
2026-07-06 20:14:20 -0300

    criando modelo de filmes

---
+++ src/Filme.java
@@ -0,0 +1,9 @@
+public class Filme {
+    // um dos atributos do filme
+    String nome;
+    int anoLancamento;
+    boolean incluidoNoPlanp;
+    double avaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+}
