cc1bff5b58380667b5126c32ea334feef5deeb91
Hiago 7k <hiago7k@gmail.com>
2026-07-15 19:58:33 -0300

    criando modificadores getters e setteres para classe serie"

--- src/br/com/alura/screenmatch/modelos/Serie.java
+++ src/br/com/alura/screenmatch/modelos/Serie.java
@@ -5,4 +5,36 @@ public class Serie extends  Titulo {
     private int episodiosPorTemporada;
     private boolean ativa;
     private int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
 }
