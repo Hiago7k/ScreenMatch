251a9089f86916141ef94aa5e7791cabb0abc3fb
Hiago 7k <hiago7k@gmail.com>
2026-07-15 19:53:59 -0300

    criando classe Serie

---
+++ src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,8 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie {
+    private int temporadas;
+    private int episodiosPorTemporada;
+    private bool ativa;
+    private int minutosPorEpisodio;
+}
