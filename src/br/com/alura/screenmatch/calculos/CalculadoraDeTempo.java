a4231f845cb9a061b72a4da546cf75a8a85c66ec
Hiago 7k <hiago7k@gmail.com>
2026-07-16 16:13:25 -0300

    polimorfismo

--- src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -7,8 +7,9 @@ public class CalculadoraDeTempo {
     public int getTempoTotal() {
         return tempoTotal;
     }
-    
+
     public void inclui(Titulo titulo){
+        System.out.println("Adicionando duração em minutos de: " + titulo);
         this.tempoTotal += titulo.getDuracaoEmMinutos();
     }
 
