ca8e45a045784478c0059b0ab9fee8dca632aeef
Hiago 7k <hiago7k@gmail.com>
2026-07-15 21:05:04 -0300

    calculadora

--- src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -1,9 +1,16 @@
 package br.com.alura.screenmatch.calculos;
 
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class CalculadoraDeTempo {
     private int tempoTotal;
 
     public int getTempoTotal() {
         return tempoTotal;
     }
+
+   public void inclui(Filme f){
+        tempoTotal += f.getDuracaoEmMinutos();
+   }
+
 }
