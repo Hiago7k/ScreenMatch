ee695dc12ce6650debed460af751b481ee01dd55
Hiago 7k <hiago7k@gmail.com>
2026-07-22 17:28:50 -0300

    criando um novo modelo

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch.principal;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+public class PrincipalComListas {
+    static void main() {
+        Filme meuFilme = new Filme("O poderoso chefão", 1972);
+        Filme seuFilme = new Filme("Titanic", 1950);
+        var filmeDoPaulo = new Filme("DogVile", 2003);
+        Serie lost = new Serie("Lost", 2015);
+
+    }
+}
