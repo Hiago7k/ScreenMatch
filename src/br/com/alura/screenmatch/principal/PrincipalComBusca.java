d5569eda140ad0fee7b0754965a520e5e7ffb8be
Hiago 7k <hiago7k@gmail.com>
2026-07-29 19:00:34 -0300

    fazendo uma requisicao API get

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,7 +1,22 @@
 package br.com.alura.screenmatch.principal;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class PrincipalComBusca {
-    static void main() {
-        
+    static void main() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=365&apikey=6f533f65"))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
     }
 }
