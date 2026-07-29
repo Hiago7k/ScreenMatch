51c1fbbe0e87b5c805ab48725183c1c088d1b28b
Hiago 7k <hiago7k@gmail.com>
2026-07-29 19:17:10 -0300

    fazendo pesquisa por terminal de filmes

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,17 +5,28 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     static void main() throws IOException, InterruptedException {
+        Scanner data = new Scanner(System.in);
+        System.out.println("Digite o Filme que deseja buscar: ");
+        String filmePesquisar = data.next();
+
+        String endereco = "https://www.omdbapi.com/?t=";
+        String endereco2 = "&apikey=6f533f65";
+
+        
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=365&apikey=6f533f65"))
+                .uri(URI.create(endereco + filmePesquisar + endereco2))
                 .build();
 
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
+        System.out.println("Trazendo informações do filme...");
         System.out.println(response.body());
 
     }
