538453c18a92720a694e863f445c71d2f430d64b
Hiago 7k <hiago7k@gmail.com>
2026-07-29 20:48:37 -0300

    fazendo desserelizacao

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,8 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -26,6 +29,11 @@ public class PrincipalComBusca {
 
         System.out.println("Trazendo informações do filme...");
         System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
 
     }
 }
