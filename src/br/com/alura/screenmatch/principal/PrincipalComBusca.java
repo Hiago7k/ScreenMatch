a04255eff1281c0a919884184b165783ef41be26
Hiago 7k <hiago7k@gmail.com>
2026-08-04 16:30:34 -0300

    Json formatado

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -27,6 +27,7 @@ public class PrincipalComBusca {
         List<Titulo> titulos = new ArrayList<>();
         Gson gson = new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
                 .create();
 
         while (!filmePesquisar.equalsIgnoreCase("sair")) {
